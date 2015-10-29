dcc63f79e82dae156720b6964494d068c211d5eb
Himanshu <himanshu.vg@gmail.com>
2015-10-30 00:36:44 +0530

    First Commit

---
+++ amifool/src/com/tcs/model/RemindMyName.java
@@ -0,0 +1,14 @@
+package com.tcs.model;
+
+public class RemindMyName {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println();
+
+	}
+
+}
